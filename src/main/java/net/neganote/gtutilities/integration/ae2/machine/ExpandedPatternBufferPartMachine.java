0e0bb8a117bc0003c52abda221548fd7ea8f84ea
jurrejelle <jurre@jilles.com>
2026-08-02 09:38:48 +0200

    Pattern buffers and proxies and the build changes I missed earlier

--- src/main/java/net/neganote/gtutilities/integration/ae2/machine/ExpandedPatternBufferPartMachine.java
+++ src/main/java/net/neganote/gtutilities/integration/ae2/machine/ExpandedPatternBufferPartMachine.java
@@ -1,25 +1,30 @@
 package net.neganote.gtutilities.integration.ae2.machine;
 
-import com.gregtechceu.gtceu.api.gui.GuiTextures;
-import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
-import com.gregtechceu.gtceu.integration.ae2.gui.widget.AETextInputButtonWidget;
-import com.gregtechceu.gtceu.integration.ae2.gui.widget.slot.AEPatternViewSlotWidget;
+import com.gregtechceu.gtceu.api.blockentity.BlockEntityCreationInfo;
+import com.gregtechceu.gtceu.common.mui.GTGuiTextures;
 import com.gregtechceu.gtceu.integration.ae2.machine.MEPatternBufferPartMachine;
-import com.gregtechceu.gtceu.integration.ae2.machine.trait.InternalSlotRecipeHandler;
-
-import com.lowdragmc.lowdraglib.gui.widget.LabelWidget;
-import com.lowdragmc.lowdraglib.gui.widget.Widget;
-import com.lowdragmc.lowdraglib.gui.widget.WidgetGroup;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 
+import appeng.api.crafting.IPatternDetails;
 import appeng.api.inventories.InternalInventory;
 import appeng.crafting.pattern.EncodedPatternItem;
+import brachy.modularui.api.drawable.Text;
+import brachy.modularui.factory.PosGuiData;
+import brachy.modularui.screen.UISettings;
+import brachy.modularui.value.sync.BooleanSyncValue;
+import brachy.modularui.value.sync.PanelSyncManager;
+import brachy.modularui.value.sync.SyncHandlers;
+import brachy.modularui.widget.ParentWidget;
+import brachy.modularui.widgets.layout.Flow;
+import brachy.modularui.widgets.layout.Grid;
+import brachy.modularui.widgets.slot.ItemSlot;
+import brachy.modularui.widgets.slot.SlotGroup;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import java.util.Arrays;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -28,105 +33,104 @@ import javax.annotation.ParametersAreNonnullByDefault;
 public class ExpandedPatternBufferPartMachine extends MEPatternBufferPartMachine {
 
     protected static final int EXPANDED_MAX_PATTERN_COUNT = 72;
+    private static final int PATTERN_COLUMNS = 9;
+    private final InternalInventory expandedTerminalInventory = new InternalInventory() {
 
-    public ExpandedPatternBufferPartMachine(IMachineBlockEntity info) {
+        @Override
+        public int size() {
+            return EXPANDED_MAX_PATTERN_COUNT;
+        }
+
+        @Override
+        public ItemStack getStackInSlot(int slotIndex) {
+            return getPatternInventory().getStackInSlot(slotIndex);
+        }
+
+        @Override
+        public void setItemDirect(int slotIndex, ItemStack stack) {
+            getPatternInventory().setStackInSlot(slotIndex, stack);
+            getPatternInventory().onContentsChanged(slotIndex);
+            onPatternChange(slotIndex);
+        }
+    };
+
+    public ExpandedPatternBufferPartMachine(BlockEntityCreationInfo info) {
         super(info);
-        this.getPatternInventory().setSize(EXPANDED_MAX_PATTERN_COUNT);
-
-        try {
-            Field internalInvField = MEPatternBufferPartMachine.class.getDeclaredField("internalInventory");
-            internalInvField.setAccessible(true);
-
-            InternalSlot[] newInternalInv = new InternalSlot[EXPANDED_MAX_PATTERN_COUNT];
-            for (int i = 0; i < EXPANDED_MAX_PATTERN_COUNT; i++) {
-                newInternalInv[i] = new InternalSlot();
-            }
-            internalInvField.set(this, newInternalInv);
-
-            Field recipeHandlerField = MEPatternBufferPartMachine.class.getDeclaredField("internalRecipeHandler");
-            recipeHandlerField.setAccessible(true);
-            recipeHandlerField.set(this, new InternalSlotRecipeHandler(this, newInternalInv));
-
-            Field internalPatternInventoryField = MEPatternBufferPartMachine.class
-                    .getDeclaredField("internalPatternInventory");
-            internalPatternInventoryField.setAccessible(true);
-            internalPatternInventoryField.set(this, new InternalInventory() {
-
-                @Override
-                public int size() {
-                    return EXPANDED_MAX_PATTERN_COUNT;
-                }
-
-                @Override
-                public ItemStack getStackInSlot(int slotIndex) {
-                    return getPatternInventory().getStackInSlot(slotIndex);
-                }
-
-                @Override
-                public void setItemDirect(int slotIndex, ItemStack stack) {
-                    getPatternInventory().setStackInSlot(slotIndex, stack);
-                    getPatternInventory().onContentsChanged(slotIndex);
-                    callPrivatePatternChange(slotIndex);
-                }
-            });
-        } catch (Exception e) {
-            throw new RuntimeException("FATAL: Failed to initialize Expanded Pattern Buffer via reflection.", e);
-        }
+        getPatternInventory().setSize(EXPANDED_MAX_PATTERN_COUNT);
+        growInternalInventory();
+        growPatternSlotDetails();
     }
 
-    private void callPrivatePatternChange(int index) {
-        try {
-            Method method = MEPatternBufferPartMachine.class.getDeclaredMethod("onPatternChange", int.class);
-            method.setAccessible(true);
-            method.invoke(this, index);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to invoke onPatternChange for index " + index, e);
+    private void growInternalInventory() {
+        InternalSlot[] current = getSuperField("internalInventory");
+        InternalSlot[] expanded = Arrays.copyOf(current, EXPANDED_MAX_PATTERN_COUNT);
+        for (int i = current.length; i < expanded.length; i++) {
+            expanded[i] = new InternalSlot();
         }
+        setSuperField("internalInventory", expanded);
     }
 
-    private String getCustomNameExpanded() {
+    private void growPatternSlotDetails() {
+        IPatternDetails[] current = getSuperField("patternSlotDetails");
+        setSuperField("patternSlotDetails", Arrays.copyOf(current, EXPANDED_MAX_PATTERN_COUNT));
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> T getSuperField(String name) {
         try {
-            Field field = MEPatternBufferPartMachine.class.getDeclaredField("customName");
+            Field field = MEPatternBufferPartMachine.class.getDeclaredField(name);
             field.setAccessible(true);
-            return (String) field.get(this);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to retrieve customName via reflection", e);
+            return (T) field.get(this);
+        } catch (ReflectiveOperationException e) {
+            throw new IllegalStateException("FATAL: Failed to read " + name + " of the pattern buffer.", e);
+        }
+    }
+
+    private void setSuperField(String name, Object value) {
+        try {
+            Field field = MEPatternBufferPartMachine.class.getDeclaredField(name);
+            field.setAccessible(true);
+            field.set(this, value);
+        } catch (ReflectiveOperationException e) {
+            throw new IllegalStateException("FATAL: Failed to expand " + name + " of the pattern buffer.", e);
         }
     }
 
     @Override
-    public Widget createUIWidget() {
-        int rowSize = 9;
-        int colSize = 8;
-        var group = new WidgetGroup(0, 0, 18 * rowSize + 16, 18 * colSize + 16);
+    public InternalInventory getTerminalPatternInventory() {
+        return expandedTerminalInventory;
+    }
 
-        for (int i = 0; i < EXPANDED_MAX_PATTERN_COUNT; i++) {
-            int index = i;
-            int x = i % rowSize;
-            int y = i / rowSize;
+    @Override
+    public void buildMainUI(ParentWidget<?> mainWidget, PosGuiData guiData, PanelSyncManager syncManager,
+                            UISettings settings) {
+        // Mirrors MEPatternBufferPartMachine#buildMainUI, with the pattern grid grown to the expanded slot count.
+        // TODO: Potentially copy more stuff from there for better UX
+        SlotGroup patternSlotGroup = new SlotGroup("pattern_slots", PATTERN_COLUMNS, 0, true);
 
-            var slot = new AEPatternViewSlotWidget(getPatternInventory(), index, 8 + x * 18, 14 + y * 18)
-                    .setOccupiedTexture(GuiTextures.SLOT)
-                    .setItemHook(stack -> {
-                        if (!stack.isEmpty() && stack.getItem() instanceof EncodedPatternItem iep) {
-                            final ItemStack out = iep.getOutput(stack);
-                            if (!out.isEmpty()) return out;
-                        }
-                        return stack;
-                    })
-                    .setChangeListener(() -> callPrivatePatternChange(index))
-                    .setBackground(GuiTextures.SLOT, GuiTextures.PATTERN_OVERLAY);
-            group.addWidget(slot);
-        }
+        BooleanSyncValue isOnlineValue = new BooleanSyncValue(this::isOnline, this::setOnline);
+        syncManager.syncValue("is_online", isOnlineValue);
 
-        group.addWidget(new LabelWidget(8, 2,
-                () -> this.isOnline ? "gtceu.gui.me_network.online" : "gtceu.gui.me_network.offline"));
+        var flow = Flow.col().coverChildren();
 
-        group.addWidget(new AETextInputButtonWidget(18 * rowSize + 8 - 70, 2, 70, 10)
-                .setText(getCustomNameExpanded())
-                .setOnConfirm(this::setCustomName)
-                .setButtonTooltips(Component.translatable("gui.gtceu.rename.desc")));
+        flow.child(Text.dynamic(() -> isOnlineValue.getBoolValue() ?
+                Component.translatable("gtceu.gui.me_network.online") :
+                Component.translatable("gtceu.gui.me_network.offline"))
+                .asWidget().marginTop(2).marginBottom(4));
 
-        return group;
+        flow.child(new Grid()
+                .height(18 * (EXPANDED_MAX_PATTERN_COUNT / PATTERN_COLUMNS))
+                .minElementMargin(0, 0)
+                .minColWidth(18).minRowHeight(18)
+                .leftRel(0.5f)
+                .gridOfSizeWidth(EXPANDED_MAX_PATTERN_COUNT, PATTERN_COLUMNS, (x, y, index) -> new ItemSlot()
+                        .slot(SyncHandlers.itemSlot(getPatternInventory(), index)
+                                .slotGroup(patternSlotGroup)
+                                .accessibility(true, true)
+                                .filter(stack -> stack.getItem() instanceof EncodedPatternItem)
+                                .changeListener((i, o, c, init) -> onPatternChange(index)))
+                        .background(GTGuiTextures.SLOT, GTGuiTextures.PATTERN_OVERLAY)));
+
+        mainWidget.child(flow.center());
     }
 }
--- src/main/java/net/neganote/gtutilities/integration/ae2/machine/ExpandedPatternBufferProxyPartMachine.java
+++ src/main/java/net/neganote/gtutilities/integration/ae2/machine/ExpandedPatternBufferProxyPartMachine.java
@@ -1,12 +1,9 @@
 package net.neganote.gtutilities.integration.ae2.machine;
 
-import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.api.blockentity.BlockEntityCreationInfo;
 import com.gregtechceu.gtceu.integration.ae2.machine.MEPatternBufferProxyPartMachine;
-import com.gregtechceu.gtceu.integration.ae2.machine.trait.ProxySlotRecipeHandler;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
-
-import java.lang.reflect.Field;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -14,15 +11,7 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @MethodsReturnNonnullByDefault
 public class ExpandedPatternBufferProxyPartMachine extends MEPatternBufferProxyPartMachine {
 
-    public ExpandedPatternBufferProxyPartMachine(IMachineBlockEntity info) {
+    public ExpandedPatternBufferProxyPartMachine(BlockEntityCreationInfo info) {
         super(info);
-        try {
-            Field handlerField = MEPatternBufferProxyPartMachine.class.getDeclaredField("proxySlotRecipeHandler");
-            handlerField.setAccessible(true);
-            handlerField.set(this,
-                    new ProxySlotRecipeHandler(this, ExpandedPatternBufferPartMachine.EXPANDED_MAX_PATTERN_COUNT));
-        } catch (Exception e) {
-            throw new RuntimeException("FATAL: Failed to initialize Expanded Proxy Recipe Handler via reflection.", e);
-        }
     }
 }
